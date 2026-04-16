3bceb3fceb6fe265f4904949299f25dac22804be
glogloria <theglocode@gmail.com>
2026-04-15 21:04:56 -0400

    Updated fillable elements

---
+++ app/Models/Product.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+#[Fillable(['sku', 'name', 'description', 'price', 'image'])]
+class Product extends Model {
+
+}
