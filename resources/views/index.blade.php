77d5cfc4f42b333c65224fc88357be166e8753b3
glogloria <theglocode@gmail.com>
2026-04-20 05:48:43 -0700

    Renamed landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,15 +24,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Log in
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">User</a></li>
-                                <li><a class="dropdown-item" href="#">Admin</a></li>
-                            </ul>
-                        </div>
+                        <li class="nav-item"><a class="nav-link" aria-current="page" href="{{ route('login') }}">Login</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Sign Up</a></li>
                     </ul>
                 </div>
