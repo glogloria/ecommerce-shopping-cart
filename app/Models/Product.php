4adb9dce949a9dcfc6a55fe76ec585acf88e7a5f
glogloria <theglocode@gmail.com>
2026-04-20 06:33:45 -0700

    Added Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 #[Fillable(['sku', 'name', 'description', 'price', 'image'])]
-class Product extends Model {
+class Product extends Model 
+{
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
 
 }
