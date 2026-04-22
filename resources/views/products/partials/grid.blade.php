ee266fec942067ff604ff617ba04b743fd1dce3e
glogloria <theglocode@gmail.com>
2026-04-22 14:26:16 -0700

    Added functionality to update or delete product

--- resources/views/products/partials/grid.blade.php
+++ resources/views/products/partials/grid.blade.php
@@ -7,12 +7,33 @@
                 @endif
 
                 <div class="card-body">
-                    <div class="container">
+                    <div class="card-container">
                         <h5 class="card-title">{{ $product->name }}</h5>
                         <h5 class="card-title">{{ $product->sku }}</h5>
                     </diV>
                     <p class="card-text">{{ $product->description }}</p>
                     <p class="fw-bold">${{ number_format($product->price, 2) }}</p>
+
+                    @auth
+                        @if(Auth::user()->role === 'admin')
+                            <div class="crud-container">
+                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-sm btn-warning"
+                                    >
+                                    Edit
+                                </a>
+
+                                <form action="{{ route('products.destroy', $product->id) }}"
+                                    method="POST"
+                                    onsubmit="return confirm('Are you sure you want to delete this product?'); ">
+                                    @csrf 
+                                    @method('DELETE')
+                                    <button class="btn btn-sm btn-danger" type="submit">
+                                        Delete
+                                    </button>
+                                </form>
+                            </div>
+                        @endif
+                    @endauth
                 </div>
             </div>
         </div>
