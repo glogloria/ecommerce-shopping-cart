d2373c328153bce48c472d91d79cfc55e02d9baa
glogloria <theglocode@gmail.com>
2026-04-22 14:32:51 -0700

    Added reference to products for product listing

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+
 // use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function admin_index() {
+        $products = Product::orderBy('id', 'desc')->get();
 
-        return view('admin.dashboard');
+        return view('admin.dashboard', compact('products'));
+    }
+
+    public function customer_index() {
+        $products = Product::orderBy('id', 'desc')->get();
+
+        return view('index', compact('products'));
     }
 }
