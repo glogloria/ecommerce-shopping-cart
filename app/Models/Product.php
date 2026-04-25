9977678ddf5578aade93b9fae7ccf8ec73380ebd
glogloria <theglocode@gmail.com>
2026-04-25 18:28:36 -0400

    Added quantity as fillable attribute

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[Fillable(['sku', 'name', 'description', 'price', 'image'])]
+#[Fillable(['sku', 'name', 'description', 'price', 'image', 'quantity'])]
 class Product extends Model 
 {
 
