23519cd882b3d0b434b5e5c0c970acbbefe458e9
glogloria <theglocode@gmail.com>
2026-04-25 18:29:23 -0400

    Updated to use prepared statements

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,20 +1,34 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     /**
      * Display form to create new user
      */
     public function register(Request $request) {
-        $incomingFields = $request->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'password' => 'required'
-        ]);
-        return 'Registration complete';
+
+    $validated = $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|max:255|unique:users, email',
+        'password' => 'required|string|max:255'
+    ]);
+
+    $password = Hash::make($validated['password']);
+
+    DB::insert(
+        "INSERT INTO users (name, email, password, role)
+        VALUES (?, ?, ?, ?)",
+        [
+            $validated['name'],
+            $validated['email'],
+            $validated['password'],
+            'customer'
+        ]
+    );
+        return response()->json(['success' => true]);
     }
 }
