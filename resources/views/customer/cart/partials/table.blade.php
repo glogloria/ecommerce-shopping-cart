d549f55e1e5c205dd01676ccdd16ba0f050e1dd3
glogloria <theglocode@gmail.com>
2026-04-28 02:28:19 -0400

    Added sku and quantity

--- resources/views/customer/cart/partials/table.blade.php
+++ resources/views/customer/cart/partials/table.blade.php
@@ -1,8 +1,10 @@
 <table class="table">
     <thead>
         <tr>
+            <th>SKU</th>
             <th>Product</th>
             <th>Price</th>
+            <th>Quantity</th>
             <th></th>
         </tr>
     </thead>
@@ -10,8 +12,10 @@
     <tbody>
         @foreach ($items as $item) 
             <tr>
+                <td>{{ number_format($item->product->sku) }}</td>
                 <td>{{ $item->product->name }}</td>
                 <td>${{ number_format($item->product->price, 2) }}</td>
+                <td>{{ number_format($item->product->quantity) }}</td>
 
                 <td>
                     <form action="{{ route('cart.remove', $item->product->id )}}" method="POST">
