9d608cddfbfeb876b7669e0d97759abc4cf5020c
glogloria <theglocode@gmail.com>
2026-04-28 02:27:34 -0400

    Added view to show profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,10 +7,18 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\View\View;
+use Illuminate\View\View;;
 
 class ProfileController extends Controller
 {
+
+    /**
+     * Display the user's profile.
+     */
+    public function show() {
+        return view('profile.show');
+    }
+    
     /**
      * Display the user's profile form.
      */
