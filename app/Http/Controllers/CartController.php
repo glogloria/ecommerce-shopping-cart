3ddebc0c23dc2f2b910ccd1dbb0bbe1a44f3b750
glogloria <theglocode@gmail.com>
2026-04-28 02:29:48 -0400

    Moved getUserCart to CartService

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -1,51 +1,54 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-use App\Models\Cart;
+use App\Services\CartService;
 use App\Models\CartItem;
 use App\Models\Product;
-use Illuminate\Validation\Rules;
 
 class CartController extends Controller
 {
-    
-    /**
-     * Get or create cart
-     */
-    private function getUserCart() {
-        return Cart::firstOrCreate([
-            'user_id' => auth()->id()
-        ]);
+
+    protected $cart;    
+ 
+    public function __construct(CartService $cart) {
+       $this->cart = $cart;
     }
 
     /**
      * View cart
      */
     public function index() {
-        $cart = $this->getUserCart();
+        $cart = $this->cart->getUserCart();
 
         $items = $cart->items()->with('product')->get();
 
-        return value('cart.index', compact('items'));
+        return view('customer.cart.index', compact('items'));
     }
-
 
     /**
      * Add item to cart
      */
-    public function add(Product $product) {
-        $cart = $this->getUserCart();
+    public function add(Request $request, Product $product) {
+        $cart = $this->cart->getUserCart();
 
-        $exists = CartItem::where('cart_id', $cart->id)
+        $request->validate([
+            'quantity' => 'required|integer|min:1|max:' . $product->quantity,
+        ]);
+
+        $cartItem = CartItem::where('cart_id', $cart->id)
             ->where('product_id', $product->id)
-            ->exists();
+            ->first();
 
-        if (! $exists) {
+        if ($cartItem) {
+            $cartItem->quantity += $request->quantity;
+            $cartItem->save();
+        } else {
             CartItem::create([
                 'cart_id' => $cart->id,
-                'product_id' => $product->id
+                'product_id' => $product->id,
+                'quantity' => $request->quantity,
+                'price' => $product->price
             ]);
         }
         
@@ -56,7 +59,7 @@ class CartController extends Controller
      * Remove product from cart
      */
     public function destroy(Product $product) {
-        $cart = $this->getUserCart();
+        $cart = $this->cart->getUserCart();
 
         CartItem::where('cart_id', $cart->id)
                 ->where('product_id', $product->id)
