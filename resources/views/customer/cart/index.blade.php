d8b6174d0c7b512baab1c7301d01f90fda23e14e
glogloria <theglocode@gmail.com>
2026-04-28 02:32:11 -0400

    Added checkout button

--- resources/views/customer/cart/index.blade.php
+++ resources/views/customer/cart/index.blade.php
@@ -4,4 +4,8 @@
 
     @include('customer.cart.partials.table', ['items' => $items])
 
+    <form action="{{ route('checkout') }}" method="POST">
+        @csrf
+        <button class="btn btn-warning btm-sm">Checkout</button>
+    </form>
 @endsection
