51c517ce14862e4125c5fcddc3cef20772427ec6
glogloria <theglocode@gmail.com>
2026-04-28 02:26:23 -0400

    Adjusted styling for price and added quantity

--- resources/views/admin/products/products-create.blade.php
+++ resources/views/admin/products/products-create.blade.php
@@ -11,7 +11,7 @@
         <input type="text" name="sku" class="w-full p-2 border rounded">
     </div>
 
-    <div>
+    <div c>
         <label class="block font-semibold">Name</label>
         <input type="text" name="name" class="w-full p-2 border rounded">
     </div>
@@ -21,19 +21,31 @@
         <textarea name="description" class="w-full p-2 border rounded"></textarea>
     </div>
 
+    <div class="price-quantity">
+        <div>
+            <label class="block font-semibold">Price</label>
+            <input type="number" name="price" step="0.01" class=" p-2 border rounded">
+        </div>
+
+        <div> 
+            <label class="block font-semibold">Quantity</label>
+            <input type="number" name="quantity" class=" p-2 border rounded">
+        </div>
+       
+    </div>
+
     <div>
         <label class="block font-semibold">Image</label>
         <input type="file" name="image" accept="../assets/*">
     </div>
 
-    <div>
-        <label class="block font-semibold">Price</label>
-        <input type="number" name="price" step="0.01" class="w-full p-2 border rounded">
-    </div>
+    <button class="w-full px-4 py-2 bg-black text-white rounded">
+        Add Product
+    </button>
 
-
-    <button class="px-4 py-2 bg-green-600 text-white rounded">
-        Save Product
+    <button id="closeModal"
+        class="w-full mt-3 bg-gray-300 py-2 rounded hover:bg-gray-400">
+        Cancel
     </button>
 </form>
 @endsection
